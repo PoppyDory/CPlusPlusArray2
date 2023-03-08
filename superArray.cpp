5b23a45e3c8eefae191c68cabb68c31cc71292d2
PoppyDory <lanza.rosita@gmail.com>
2023-03-08 14:11:37 +0100

    errore brutto

--- superArray.cpp
+++ superArray.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int *aggiungiInFondo(int base[], int lunghezza, int nuovo)
 {
     int lunghezzaNuovo = lunghezza + 1;
-    int arrayNuovo[lunghezzaNuovo];
+    int *arrayNuovo = new int[lunghezzaNuovo];
+
     for (int i = 0; i < lunghezzaNuovo; i++)
     {
         arrayNuovo[i] = base[i];
@@ -26,7 +27,7 @@ int *aggiungiInFondo(int base[], int lunghezza, int nuovo)
 int *aggiungiInTesta(int base[], int lunghezza, int nuovo)
 {
     int lunghezzaNuovo = lunghezza + 1;
-    int arrayNuovo[lunghezzaNuovo];
+    int *arrayNuovo = new int[lunghezzaNuovo];
 
     arrayNuovo[0] = nuovo;
 
@@ -48,7 +49,8 @@ int *aggiungiInTesta(int base[], int lunghezza, int nuovo)
 int *aggiungiInPosizione(int base[], int lunghezza, int nuovo, int posizione)
 {
     int lunghezzaNuovo = lunghezza + 1;
-    int arrayNuovo[lunghezzaNuovo];
+    cout << "test3" << endl;
+    int *arrayNuovo = new int[lunghezzaNuovo];
 
     for (int i = 0; i < posizione; i++)
     {
@@ -87,7 +89,7 @@ int trovaPosizione(int base[], int lunghezza, int cerca)
 
 int *aggiornaInPosizione(int base[], int lunghezza, int valore, int posizione)
 {
-    int arrayNuovo[lunghezza];
+    int *arrayNuovo = new int[lunghezza];
     for (int i = 0; i < posizione; i++)
     {
         arrayNuovo[i] = base[i];
@@ -113,14 +115,15 @@ int *aggiornaInPosizione(int base[], int lunghezza, int valore, int posizione)
 int *rimuovi(int base[], int lunghezza, int indice)
 {
     int lunghezzaNuovo = lunghezza - 1;
-    int arrayNuovo[lunghezzaNuovo];
-    for (int i = 0; i < indice; i++) //finche non arrivo alla posizione dell'elento che voglio eliminare scrivo l'array come era prima
+    int *arrayNuovo = new int[lunghezzaNuovo];
+
+    for (int i = 0; i < indice; i++) // finche non arrivo alla posizione dell'elento che voglio eliminare scrivo l'array come era prima
     {
-            arrayNuovo[i] = base[i];
+        arrayNuovo[i] = base[i];
     }
-    for (int i = indice; i < lunghezzaNuovo; i++)//quando arrivo alla posizione dell'elemento che voglio togliere salto la copia 
+    for (int i = indice; i < lunghezzaNuovo; i++) // quando arrivo alla posizione dell'elemento che voglio togliere salto la copia
     {
-        arrayNuovo[i] = base[i+1];// con base i+1 salto l'elemento che voglio toglire  
+        arrayNuovo[i] = base[i + 1]; // con base i+1 salto l'elemento che voglio toglire
     }
 
     cout << endl
@@ -131,14 +134,51 @@ int *rimuovi(int base[], int lunghezza, int indice)
     }
     return arrayNuovo;
 }
+int *rimuoviNumero(int base[], int lunghezza, int elemento)
+{
+    int contatore = 0;
+
+    for (int i = 0; i < lunghezza; i++)
+    {
+        if (base[i] == elemento)
+        {
+            contatore++;
+        }
+    }
+    int *arrayNuovo = new int[lunghezza - contatore];
+    for (int i = 0; i < lunghezza; i++)
+    {
+        int indice;
+        if (base[i] == elemento)
+        {
+            indice = i;
+            for (int i = 0; i < indice; i++)
+            {
+                arrayNuovo[i] = base[i];
+            }
+            for (int i = indice; i < lunghezza; i++)
+            {
+                arrayNuovo[i] = base[i + 1];
+            }
+        }
+    }
+    cout << endl
+         << "Elimina l'elemento " << elemento << endl;
+    for (int i = 0; i < lunghezza - contatore; i++)
+    {
+        cout << arrayNuovo[i] << " ";
+    }
+    return arrayNuovo;
+}
 
 int main()
 {
-    int arrayBase[] = {4, 5, 3, 7, 6};
+    int arrayBase[] = {4, 5, 3, 7, 7, 1, 6};
     int lunghezzaBase = sizeof(arrayBase) / sizeof(arrayBase[0]);
     int numero = 8;
     int posizione = 1;
     int cerca = 6;
+    int elemento = 7;
 
     int *arrayAggiungiInFondo = aggiungiInFondo(arrayBase, lunghezzaBase, numero);
     cout << endl
@@ -146,7 +186,9 @@ int main()
     int *arrayAggiungiInTesta = aggiungiInTesta(arrayBase, lunghezzaBase, numero);
     cout << endl
          << "------------" << endl;
+    cout << "test1" << endl;
     int *arrayAggiungiInPosizione = aggiungiInPosizione(arrayBase, lunghezzaBase, numero, posizione);
+    cout << "test2" << endl;
     cout << endl
          << "------------" << endl
          << "Il numero " << cerca << " si trova il posizione " << trovaPosizione(arrayBase, lunghezzaBase, cerca) << endl
@@ -155,4 +197,7 @@ int main()
     cout << endl
          << "------------" << endl;
     int *arrayRimuovi = rimuovi(arrayBase, lunghezzaBase, posizione);
+    cout << endl
+         << "------------" << endl;
+    int *arrayRimuoviNumero = rimuoviNumero(arrayBase, lunghezzaBase, elemento);
 }
